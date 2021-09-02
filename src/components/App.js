7b1300d63021f8a5f6defbf8a7240a620022ef29
kridt <chrnielsen2003@gmail.com>
2021-09-02 13:40:38 +0200

    works

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
+import StartSide from "./StartSide"
 
 function App() {
 
@@ -16,16 +17,14 @@ function App() {
   });
  */
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+    <Container>
+      <div>
         <Router>
           <AuthProvider>
             <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route exact path="/" component={StartSide} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
