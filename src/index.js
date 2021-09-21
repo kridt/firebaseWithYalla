91e3d086b6f57e3ede7c6c2aa46ac5103a951690
Christian <chrnielsen2003@gmail.com>
2021-09-21 18:41:07 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -19,9 +19,4 @@ ReactDOM.render(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
-<<<<<<< HEAD
 } */
-=======
-}
- */
->>>>>>> 667ad5e8fe66e5c2b743ff8cb1b881abda864106
